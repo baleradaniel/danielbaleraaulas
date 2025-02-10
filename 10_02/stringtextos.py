84ebb68b16100acc7f8d98f206910766cf478b96
baleradaniel <danielbalera021@gmail.com>
2025-02-10 20:32:04 -0300

    python

---
+++ 10_02/stringtextos.py
@@ -0,0 +1,7 @@
+email = 'danielbalera021@gmail.com'
+
+print('\nPegando posição ZERO no email atribuido:')
+print(email[0])
+
+print('\nPegando da posição ZERO até a ONZE do email atribuido:')
+print(email[0:11])
