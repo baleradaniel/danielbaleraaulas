fff851ba6b9320ff38d3d748657babef78ef804d
baleradaniel <danielbalera021@gmail.com>
2025-02-10 22:07:57 -0300

    sim

--- 10_02/stringtextos.py
+++ 10_02/stringtextos.py
@@ -1,12 +1,11 @@
-"""email = 'danielbalera@gmail.com'
+email = 'danielbalera@gmail.com'
 
 print('\nPegando posição ZERO no email atribuido:')
 print(email[0])
 
 print('\nPegando da posição ZERO até a DOZE do email atribuido:')
-print(email[0:13])"""
+print(email[0:13])
 
-email = 'danielbalera@gmail.com'
 
 print('\nUsando metodo LEN para saber o tamanho da variavel:')
 print(len(email))
