85248b73e13fd6b3f517e3596a2aef923301146a
baleradaniel <danielbalera021@gmail.com>
2025-02-20 19:03:47 -0300

    Create tupla.py

---
+++ tuplas/tupla.py
@@ -0,0 +1,7 @@
+vendas = ('Lira','25/08/1010','15/02/1994',2000,'Estagiario')
+nome, data_contratacao, data_nascimento, salario, cargo = vendas
+print(nome)
+print(data_contratacao)
+print(data_nascimento)
+print(salario)
+print(cargo)
